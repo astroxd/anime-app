0a532c1a37e3290da99d444410e67009a7a1e283
Andrea Sciortino <andreasciortino2004@gmail.com>
2021-07-23 15:32:26 +0200

    nav bar

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,18 @@
 <script>
-	export let name;
+	import NavBar from './NavBar.svelte'
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<!-- <Testbar/> -->
+	<NavBar/>
+	<div class="main-content">
+		<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio saepe nemo sit quibusdam ipsum soluta, ut nostrum voluptatem porro doloribus minima perferendis ratione vel natus recusandae dolores necessitatibus temporibus veritatis veniam praesentium ducimus laudantium. Consequuntur beatae odio labore amet possimus! Doloribus consectetur, perferendis praesentium maxime explicabo beatae dolorum pariatur, voluptatem aliquam delectus labore, exercitationem architecto? Sequi cum dicta fugiat quibusdam iusto error ipsum reprehenderit quidem dolorem dolor, sit nihil enim earum doloremque voluptatem molestiae! Fuga suscipit sint illum sunt architecto quod provident dolorum. Modi dolores similique cum aliquam, animi culpa eum illo sunt voluptatibus molestias deserunt unde blanditiis? Pariatur, ab!</p>
+	</div>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+	.main-content{
+  		margin-left: 8rem;
+  		padding: 1rem;
 	}
 </style>
